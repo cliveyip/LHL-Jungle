7768e559c7ad2baf94688ef68482294638e9ec55
Clive Yip <yipsifu@gmail.com>
2016-11-15 16:58:45 +0000

    added order number in order receipt email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   default from: 'no-reply@jungle.com'
   def order_receipt_email(order)
     @order = order
-    mail(to: 'yipsifu@gmail.com', subject: "Order Receipt")
+    mail(to: 'yipsifu@gmail.com', subject: "Order Receipt for Order: #{@order.id}")
   end
 end
